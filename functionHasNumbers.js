b84484130a07c3db62986c5bebb094b660b019f2
AlinaZakirod <azakirova@reloquest.com>
2019-07-28 11:24:01 -0400

    has a number?

---
+++ functionHasNumbers.js
@@ -0,0 +1,12 @@
+let myString = 'sjcohwiurvbhw12'
+
+let includeNumber;
+
+function (aString){
+    for (let i = 0; i< aString.lenght; i++){
+        if(aString[i] === 0 || aString[i] === 1 || aString[i] === 2 || aString[i] === 3  || aString[i] === 4 || aString[i] === 5 || aString[i] === 6 || aString[i] === 7 || aString[i] === 8 || aString[i] === 9)){
+            return includeNumber = true
+        } else 
+        {return includeNumber = false};
+    }
+}   
